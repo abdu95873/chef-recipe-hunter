49dac1b26795f195d5085cdf407183cbd2d59f1c
abdu95873 <abdu95873@gmail.com>
2023-05-02 11:06:46 +0600

    git and google login done

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -4,6 +4,7 @@ import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../Providers/AuthProvider';
 
 
+
 const Login = () => {
     
     const [show, setShow] =useState(false);
@@ -13,10 +14,16 @@ const Login = () => {
 
 
 
-    const { signIn } = useContext(AuthContext);
+    const { signIn, googleLogin, gitLogin } = useContext(AuthContext);
     const navigate = useNavigate();
     const location = useLocation();
     const from = location.state?.from?.pathname || "/";
+    
+    
+
+
+
+
 
     const handleLogin = event => {
         event.preventDefault();
@@ -40,6 +47,42 @@ const Login = () => {
             setError(error.message);
           })
       }
+
+      const handleGoogleLogin = ()=>{
+        googleLogin()
+        .then(result => {
+            const loggedInUser = result.user;
+            console.log(loggedInUser);
+            navigate(from, {replace:true})
+            setSuccess('Successfully login');
+    
+          })
+          .catch(error => {
+            console.log(error);
+            setError(error.message);
+          })
+
+      } 
+      const handleGitLogin = ()=>{
+        gitLogin()
+        .then(result => {
+            const loggedInUser = result.user;
+            console.log(loggedInUser);
+            navigate(from, {replace:true})
+            setSuccess('Successfully login');
+    
+          })
+          .catch(error => {
+            console.log(error);
+            setError(error.message);
+          })
+
+      }
+
+
+
+
+
     return (
         <Container className='my-4 py-5 mx-auto w-25'>
       <h4 className='text-center'>please login</h4>
@@ -55,6 +98,14 @@ const Login = () => {
           <Form.Control type="password" name='password' placeholder="Password" required />
         </Form.Group>
 
+        <p onClick={() => setShow(!show)}><small>
+                        { 
+                        show ? <span>Hide password</span>: <span>Show password</span>
+                        
+                    }
+                        </small></p>
+
+
         <Button variant="primary" type="submit" className='d-block mx-auto'>
           Login
         </Button>
@@ -67,6 +118,12 @@ const Login = () => {
 
         <Form.Text className='text-center mx-auto'>Donn't have an account? <Link to="/register" className='text-danger'>Register</Link></Form.Text>
       </Form>
+
+      <h5>Login with ....</h5>
+      
+      <button className='me-3' onClick={handleGoogleLogin}>google</button>
+      <button onClick={handleGitLogin}>git</button>
+    
     </Container>
     );
 };
--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -1,6 +1,7 @@
-import React, { createContext, useState } from 'react';
-import {getAuth} from "firebase/auth";
+import React, { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup} from "firebase/auth";
 import app from '../Firebase/firebase.config';
+import { GithubAuthProvider, GoogleAuthProvider} from 'firebase/auth';
 
 
 
@@ -9,10 +10,57 @@ const auth = getAuth(app);
 
 const AuthProvider = ({children}) => {
 
+    const googleProvider = new GoogleAuthProvider();
+    const gitProvider = new GithubAuthProvider();
+
     const [user,setUser] = useState(null);
-    const authInfo = {
-        user
+    const [loading, setLoading] = useState(true);
+
+    const createUser= (email, password) =>{
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
     }
+
+    const signIn = (email, password) =>{
+
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    const googleLogin = () =>{
+        return signInWithPopup(auth, googleProvider);  
+    }
+
+    const gitLogin = () =>{
+        return signInWithPopup (auth,gitProvider);
+    }
+
+
+    const logOut = () =>{
+        return signOut(auth);
+    }
+    useEffect( () =>{
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) =>{
+            setUser(currentUser);
+            setLoading(false);
+        });
+        return ()=>{
+            return unsubscribe();
+        }
+    },[])
+
+    
+    const authInfo ={
+        user,
+        loading,
+        createUser,
+        signIn,
+        logOut,
+        googleLogin,
+        gitLogin
+        
+        
+    };
 
     return (
         <AuthContext.Provider value={authInfo}>
